4cd790c0f4af5a0426df64bae90b6a9ce61a2f08
Vishnu8122003 <98574539+Vishnu8122003@users.noreply.github.com>
2026-03-22 21:31:59 +0530

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -129,9 +129,15 @@ class YouTube:
             "nocheckcertificate": True,
             "cookiefile": cookie,
             "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "http_headers": {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Referer": "https://www.google.com/",
+            },
             "extractor_args": {
                 "youtube": {
                     "player_client": ["android", "web"],
+                    "player_skip": ["webpage", "configs"],
                 }
             },
         }
